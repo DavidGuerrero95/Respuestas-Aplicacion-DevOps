76aaf2135bf430fb136a3e1c36d53eb5fb237c27
Santiago <davids.guerrero@udea.edu.co>
2022-08-23 19:58:00 -0500

    Cambios

--- src/main/java/com/app/respuestas/controller/RespuestaController.java
+++ src/main/java/com/app/respuestas/controller/RespuestaController.java
@@ -197,6 +197,14 @@ public class RespuestaController {
 		return rRepository.findAll();
 	}
 
+	@DeleteMapping("/respuestas/eliminar-preguntas-problema")
+	public Boolean eliminarPreguntasProblema(){
+		rRepository.deleteByIdProyecto(2);
+		rRepository.deleteByIdProyecto(6);
+		rRepository.deleteByIdProyecto(17);
+		return true;
+	}
+
 	@DeleteMapping("/respuestas/eliminar/proyecto/formulario/{idProyecto}")
 	public Boolean eliminarRespuestasProyectoFormulario(@PathVariable("idProyecto") Integer idProyecto,
 			@RequestParam(value = "formulario", defaultValue = "1") Integer formulario) throws IOException {
